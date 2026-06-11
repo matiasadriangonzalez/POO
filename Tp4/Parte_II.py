673d592fe8c89d97df6a54605c6c32c43277a2b1
matias <matias.09adrian@gmail.com>
2026-06-10 22:40:30 -0300

    ejercicio 3

--- Tp4/Parte_II.py
+++ Tp4/Parte_II.py
@@ -1,9 +1,17 @@
 # Superclase
 class Archivo:
     def __init__(self, nombre: str, peso_en_mb: float):
-        self.nombre = nombre
-        self.peso_en_mb = peso_en_mb
+        self.__nombre = nombre           
+        self.__peso_en_mb = peso_en_mb   
         print("Creando Archivo genérico...")
+
+    # Getters para acceder desde afuera
+    def get_nombre(self):
+        return self.__nombre
+
+    def get_peso_en_mb(self):
+        return self.__peso_en_mb
+
 
 # Subclase
 class ArchivoVideo(Archivo):
@@ -11,13 +19,14 @@ class ArchivoVideo(Archivo):
         super().__init__(nombre, peso_en_mb)
         print("Creando Archivo de Video...")
 
+
 if __name__ == "__main__":
 
     # CASO 1: con super() — funciona correctamente
     print("--- CASO 1: CON super() ---")
     video = ArchivoVideo("pelicula.mp4", 1200.5)
-    print(f"Nombre: {video.nombre}")
-    print(f"Peso: {video.peso_en_mb} MB")
+    print(f"Nombre: {video.get_nombre()}")
+    print(f"Peso: {video.get_peso_en_mb()} MB")
 
     print()
 
@@ -32,6 +41,6 @@ if __name__ == "__main__":
     video2 = ArchivoVideoSinSuper("pelicula.mp4", 1200.5)
 
     try:
-        print(f"Nombre: {video2.nombre}")  # 💥 AttributeError
+        print(f"Nombre: {video2.get_nombre()}")  # 💥 AttributeError
     except AttributeError as e:
         print(f"ERROR: {e}")
